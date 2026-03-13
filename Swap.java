e0aa5a423ac4f022179a1e25e603b7635c19c5fe
Md Zaidur Rahman <110111135+zaidur12@users.noreply.github.com>
2026-03-14 00:53:10 +0530

    problem3

---
+++ Swap.java
@@ -0,0 +1,13 @@
+public class Swap {
+     public static void main(String[] args){
+        int a = 5;
+        int b = 9;
+
+        int c = a;
+        a = b;
+        b = c;
+
+        System.out.println("Value of a = " + a +"Value of b = " + b);
+     }
+    
+}
