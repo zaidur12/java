ef9a691462df03444450eb4e9f042885b2a92961
Md Zaidur Rahman <110111135+zaidur12@users.noreply.github.com>
2026-03-14 01:46:16 +0530

    problem9

---
+++ Dib.java
@@ -0,0 +1,10 @@
+public class Dib {
+    public static void main(String[] args){
+        double g = 9.9;
+        int a = (int)g;
+        byte b = (byte)g;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
