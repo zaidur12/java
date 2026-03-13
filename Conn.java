01644e9b7aca71fcc493babce175fac22682e4ba
Md Zaidur Rahman <110111135+zaidur12@users.noreply.github.com>
2026-03-14 01:39:31 +0530

    problem8

---
+++ Conn.java
@@ -0,0 +1,7 @@
+public class Conn {
+    public static void main(String[] args){
+        char ch = '5';
+        int a = Integer.parseInt(String.valueOf(ch));
+        System.out.println(a);
+    }
+}
