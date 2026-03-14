a77a70b8f09a987c819a7cbfc62d7c3dcb2db2b6
Md Zaidur Rahman <110111135+zaidur12@users.noreply.github.com>
2026-03-14 16:25:03 +0530

    problem15

---
+++ conca.java
@@ -0,0 +1,8 @@
+public class conca {
+     public static void main(String[] args){
+        String a = "hello ";
+        String b = "how r u ";
+        String c = "where are you from";
+        System.out.println(a.concat(b).concat(c));
+    }
+}
