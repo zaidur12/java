0e6a9317f54de3a96a1572d31db9bbacc8d227ec
Md Zaidur Rahman <110111135+zaidur12@users.noreply.github.com>
2026-03-15 13:10:14 +0530

    problem19

---
+++ classs.java
@@ -0,0 +1,16 @@
+class Cal
+{
+  public int add(int a, int b){
+  return a+b;
+  }
+} 
+
+public class classs {
+     
+    public static void main(String[] args){
+        Cal calc = new Cal();
+        int result = calc.add(4,5);
+        System.out.println(result);
+
+    }
+}
