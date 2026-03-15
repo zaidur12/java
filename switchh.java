3d4134a7971fdf007d60f51ba5b0c07977cb64cd
Md Zaidur Rahman <110111135+zaidur12@users.noreply.github.com>
2026-03-15 12:54:49 +0530

    problem17

---
+++ switchh.java
@@ -0,0 +1,14 @@
+public class switchh {
+    public static void main(String[] args){
+        int n = 2;
+        switch(n){
+            case 1:
+                System.out.println("hello");
+                break;
+            case 2:
+                System.out.println("world");
+                break;
+
+        }
+    }
+}
