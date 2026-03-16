25864a0785d082797b17fd0fa2329990f53b73c1
Md Zaidur Rahman <110111135+zaidur12@users.noreply.github.com>
2026-03-16 23:43:37 +0530

    uf

---
+++ dup.java
@@ -0,0 +1,13 @@
+public class dup {
+    public static void main(String[] args){
+        int arr [] = { 1,1,5,7,5,8};
+        for(int i=0;i<arr.length;i++){
+          for(int j = 1;j<arr.length;j++){
+             if(arr[i]== arr[j]){
+               
+             }
+          }
+        }
+
+    }
+}
