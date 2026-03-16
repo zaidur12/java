c8d5936174d883836c3996b2a740cf607216e645
Md Zaidur Rahman <110111135+zaidur12@users.noreply.github.com>
2026-03-16 21:36:39 +0530

    copy

---
+++ copy.java
@@ -0,0 +1,9 @@
+public class copy {
+    public static void main(String[] args){
+        int arr [] = {2,6,7,2,1};
+        int arr1[] = arr.clone();
+        for(int i =0 ; i<arr.length;i++){
+           System.out.println(arr1[i]);
+        } 
+}
+}
