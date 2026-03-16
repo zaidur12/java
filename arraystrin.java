c0a492c5d3359cb1235b5cff6d1b1c03f40f0c38
Md Zaidur Rahman <110111135+zaidur12@users.noreply.github.com>
2026-03-16 21:01:46 +0530

    String array

---
+++ arraystrin.java
@@ -0,0 +1,8 @@
+public class arraystrin {
+    public static void main(String[] args){
+        String arr [] = {"Avishek","Sanju","Ishan"};
+        for(int i = 0;i < arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
