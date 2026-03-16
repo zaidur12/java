6fdb41edc8d9606fb29bf4e035d13f15364b60d9
Md Zaidur Rahman <110111135+zaidur12@users.noreply.github.com>
2026-03-16 21:21:17 +0530

    reverse

---
+++ reve.java
@@ -0,0 +1,10 @@
+public class reve {
+    public static void main(String[] args){
+        int arr [] = {2,6,7,2,1};
+        for(int i =arr.length-1;i>=0;i--){
+           System.out.println(arr[i]);
+        }
+        
+        
+    }
+}
