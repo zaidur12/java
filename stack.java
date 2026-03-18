018f562c9dff0af01c820e2949348e64b0980baa
Md Zaidur Rahman <110111135+zaidur12@users.noreply.github.com>
2026-03-18 22:31:14 +0530

    .

--- stack.java
+++ stack.java
@@ -1,12 +1,13 @@
 import java.util.Stack;
 
 public class stack {
-    public static void main(String[] args){
-        Stack<Integer> number = new Stack<>();
+    public static void main(String[] args) {
+        Stack<Integer> number = new Stack<Integer>();
         number.push(9);
         number.push(8);
+        number.push(7);
+        number.push(6);
 
-
-        System.err.println(number.peek());
+        System.err.println(number);
     }
 }
