76ea2dbf9f9049ac00afcfd026da2a1eeedbd3e8
Md Zaidur Rahman <110111135+zaidur12@users.noreply.github.com>
2026-03-18 21:54:54 +0530

    arraylistt

--- aaraylist.java
+++ aaraylist.java
@@ -9,6 +9,16 @@ public class aaraylist {
         student.add("Derek");
         student.add("Even");
 
+        student.add(1,"ZR");
+         ArrayList<String> student1 = new ArrayList<>();
+        student1.add("Alex");
+        student1.add("Blake");
+        student1.add("Camroon");
+        student1.add("Derek");
+        student1.add("Even");
+
+        student.add(1,"ZR");
+
         System.out.println(student);
 
     }
