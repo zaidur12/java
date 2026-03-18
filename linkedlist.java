a029cb56b1cf6f0888a22177097ac8b4da02e428
Md Zaidur Rahman <110111135+zaidur12@users.noreply.github.com>
2026-03-18 22:33:50 +0530

    linkedlist

---
+++ linkedlist.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+
+public class linkedlist {
+      public static void main(String[] args){
+        
+        
+        LinkedList<String> l = new LinkedList<String>();
+
+    
+        l.add("One");
+        l.add("Two");
+        l.add("Three");
+        l.add("Four");
+        l.add("Five");
+
+        System.out.println(l);
+    }
+}
