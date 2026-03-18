5794bee6e240c7ee74c5da6d466de3a971dbdeb7
Md Zaidur Rahman <110111135+zaidur12@users.noreply.github.com>
2026-03-18 21:49:24 +0530

    arraylist

---
+++ aaraylist.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class aaraylist {
+    public static void main(String[] args) {
+        ArrayList<String> student = new ArrayList<>();
+        student.add("Alex");
+        student.add("Blake");
+        student.add("Camroon");
+        student.add("Derek");
+        student.add("Even");
+
+        System.out.println(student);
+
+    }
+}
