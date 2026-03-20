575d4c8f47897166014a495c09edc65886bbee28
Md Zaidur Rahman <110111135+zaidur12@users.noreply.github.com>
2026-03-21 00:00:24 +0530

    generic extend

--- genmethod.java
+++ genmethod.java
@@ -2,8 +2,13 @@ public class genmethod {
     public static void main(String[] args){
       printdata("hello");
       printdata(123);
+      Strr("Hi");
+     
     }
     static <E> void printdata( E data){
       System.out.println(data);
+    }
+     static <E extends String> void Strr (E data){
+        System.out.println(data);
     }
 }
