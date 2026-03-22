caa8e653eebded329259323f3671439a0522fa39
Md Zaidur Rahman <110111135+zaidur12@users.noreply.github.com>
2026-03-22 20:38:12 +0530

    function

--- function.java
+++ function.java
@@ -1,5 +1,8 @@
+import java.util.function.Function;
+
 public class function {
     public static void main(String[] args){
-        
+        Function<Integer,Integer> doub = x -> x*2;
+        System.out.println(doub.apply(100));
     }
 }
