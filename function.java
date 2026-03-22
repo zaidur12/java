4dcc988877878847feabbf7cdc175e8e80f06077
Md Zaidur Rahman <110111135+zaidur12@users.noreply.github.com>
2026-03-22 20:42:19 +0530

    function

--- function.java
+++ function.java
@@ -4,5 +4,7 @@ public class function {
     public static void main(String[] args){
         Function<Integer,Integer> doub = x -> x*2;
         System.out.println(doub.apply(100));
+        doub = doub.andThen(x-> x/4);
+        System.out.println(doub.apply(100));
     }
 }
