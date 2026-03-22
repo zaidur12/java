07314ca3650b9a7b603812036b91e5608927b3d7
Md Zaidur Rahman <110111135+zaidur12@users.noreply.github.com>
2026-03-22 20:58:17 +0530

    Consumer

--- consumer.java
+++ consumer.java
@@ -1,8 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class consumer {
     public static void main(String[] args){
         Consumer<Integer> display = x -> System.out.println(x);
         display.accept(100);
+        
+        Consumer<List<Integer> > modify = list ->
+        {
+            for (int i = 0; i < list.size(); i++)
+                list.set(i, 2 * list.get(i));
+        };
+
+       
+        Consumer<List<Integer> >
+            dispList = list -> list.stream().forEach(a -> System.out.print(a + " "));
+
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(2);
+        list.add(1);
+        list.add(3);
+
+        modify.accept(list);
+
+        
+        dispList.accept(list);
     }
 }
+    
+
