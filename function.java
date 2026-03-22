073138e83ef76d1001e194616bebf73f7e6cd5cb
Md Zaidur Rahman <110111135+zaidur12@users.noreply.github.com>
2026-03-22 20:32:04 +0530

    predicate

---
+++ function.java
@@ -0,0 +1,5 @@
+public class function {
+    public static void main(String[] args){
+        
+    }
+}
--- predicate.java
+++ predicate.java
@@ -5,6 +5,10 @@ public class predicate {
     
     Predicate<Integer> isEven = x -> x%2==0;
     System.out.println(isEven.test(4));
+    Predicate<String> word = x -> x.startsWith(("A"));
+    Predicate<String> end = x -> x.endsWith("t");
+    Predicate<String> and = word.and(end);
+    System.out.println(and.test("Ankit"));
     }
 }
 
