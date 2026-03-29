8b50ed84f669053fe9906a1fd16b8dca08dc949e
Md Zaidur Rahman <110111135+zaidur12@users.noreply.github.com>
2026-03-29 20:44:37 +0530

    collection

--- srtudent.java
+++ srtudent.java
@@ -1,3 +1,17 @@
-public t  {
-    
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class srtudent {
+    public static void main(String[] args) {
+        
+        Map<String,Integer> students= new HashMap<>();
+        students.put("Maths", 98);
+        students.put("English", 98);
+        students.put("Science",78);
+
+        System.out.println(students);
+        System.out.println(Collections.max(students.values()));
+
+    }
 }
